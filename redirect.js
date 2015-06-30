d95602dfdd51aed6bb005562bd5f5283b34b6b1c
unknown <joaquin.ocampo@GLB-D3782.globant.com>
2015-06-30 18:06:04 -0300

    background and backButton in mainPage

--- redirect.js
+++ redirect.js
@@ -6,3 +6,8 @@ function mainRedirect() {
     var myWindow = window.open("/home/jakita/Proyecto1/main.html","main_page", "_self");
     myWindow.document.write("<p>I replaced the current window.</p>");
 }
+
+function backHomePage() {
+	var myWindow = window.open("/home/jakita/Proyecto1/homePage.html","home_page", "_self");
+    myWindow.document.write("<p>I replaced the current window.</p>");	
+}
