b2497f0c0340c91a494ed061b7546167ff20efc4
unknown <joaquin.ocampo@GLB-D3782.globant.com>
2015-07-01 19:23:14 -0300

    add video to home page

--- redirect.js
+++ redirect.js
@@ -3,11 +3,11 @@
 }*/
 
 function mainRedirect() {
-    var myWindow = window.open("/home/jakita/Proyecto1/main.html","main_page", "_self");
+    var myWindow = window.open("https://github.com/jakax/Proyecto1/blob/master/main.html","main_page", "_self");
     myWindow.document.write("<p>I replaced the current window.</p>");
 }
 
 function backHomePage() {
-	var myWindow = window.open("/home/jakita/Proyecto1/homePage.html","home_page", "_self");
+	var myWindow = window.open("https://github.com/jakax/Proyecto1/blob/master/homePage.html","home_page", "_self");
     myWindow.document.write("<p>I replaced the current window.</p>");	
 }
